3623edc3c2cb4e7eac61b4b9b40d0fbceff772c5
Girmay <ink423103@yahoo.com>
2019-11-02 13:04:25 +0100

    Updates

---
+++ precache-manifest.6eb36840fc1ad40580b6e5517b7581d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4028a83d2aeaab3c9a154a4601e05d",
+    "url": "/githubSearch/index.html"
+  },
+  {
+    "revision": "dd88d830b7d796ab0cc3",
+    "url": "/githubSearch/static/css/main.bb17aa4c.chunk.css"
+  },
+  {
+    "revision": "2cf497fcf1b0c4ba4cdb",
+    "url": "/githubSearch/static/js/2.12a394e7.chunk.js"
+  },
+  {
+    "revision": "dd88d830b7d796ab0cc3",
+    "url": "/githubSearch/static/js/main.7e8b4c37.chunk.js"
+  },
+  {
+    "revision": "124d01a60ea281727e91",
+    "url": "/githubSearch/static/js/runtime-main.57601800.js"
+  }
+]);
